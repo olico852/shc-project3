00320b1ee62bd39417f47e3e8a71a3b54024a03f
top-banana <larissa.typ@gmail.com>
2017-02-16 11:21:23 +0800

    larissa changes

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,7 +6,23 @@ class Search < ApplicationRecord
     # caregivers = caregivers.where(["specialties LIKE ?", specialties]) if specialties.present?
     # caregivers = caregivers.where(["yearsofexperience >= ?", yearsofexperience]) if yearsofexperience.present?
     # caregivers = caregivers.where(["gender LIKE ?", gender]) if gender.present?
-
+    caregivers = Caregiver.where('id IN (SELECT DISTINCT caregiver_id FROM caregivers_languages WHERE language_id = language.find_by(sglang))')
     return caregivers
   end
 end
+
+# SELECT DISTINCT c.id
+# FROM caregivers AS c
+# INNER JOIN caregivers_languages AS cl ON (cl.caregiver_id = c.id)
+# INNER JOIN languages AS l ON (l.id = cl.language_id)
+# WHERE l.sglang = 'Chinese' ||'English';
+#
+#
+# users = User.where('id IN (SELECT DISTINCT user_id FROM bars WHERE bars.foo_id!=?)', 5)
+#
+#
+# caregivers = Caregiver.where('id IN (SELECT DISTINCT caregiver_id FROM caregivers_languages WHERE language_id = language.find_by(lang))')
+#
+# caregivers = Caregiver.where('id IN (SELECT DISTINCT caregiver_id FROM caregivers_languages WHERE language_id = language.find_by(id: 1))')
+#
+# caregivers = caregiver.where('id IN (SELECT DISTINCT caregiver_id FROM caregivers_languages WHERE language_id = language.find_by(id: 1))')
